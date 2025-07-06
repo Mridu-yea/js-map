01a9a499531b9495989173296d344c64e2acf5bc
Mridu <kimpeak.2149@gmail.com>
2025-07-06 16:18:55 +0000

    config commit

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+let name = 'mue' // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+// number range => 2^53 
+// very big num => bigint
+// string => '' ""
+// boolean => true/false
+// null [object]=> standalone value | no value at all/empty/not undefined
+// undefined [undef] => no value defined yet/not empty
+// symbol => unique 
+
+// object 
+
+console.log(typeof name);// string
+
+
+
