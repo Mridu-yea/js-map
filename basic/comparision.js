d4c6e9b853f4c3f83114e3da7c6ddece7f0b0028
Mridu <kimpeak.2149@gmail.com>
2025-07-09 17:36:35 +0000

    Comparision done

---
+++ basic/comparision.js
@@ -0,0 +1,59 @@
+//? SAME DATA TYPE :
+console.log(2 < 1);  // False  
+console.log(2 > 1);  // True 
+console.log(2 == 1); // False 
+console.log(2 <= 1); // False
+console.log(2 >= 1); // True
+console.log(2 != 1); //True
+
+console.log("a" < "a");  //False
+console.log("a" == "a"); //True
+
+console.log(true > false);  //True
+console.log(true == false); //False 
+
+
+
+//? DIFFERENT DATA TYPE :
+console.log("2" == 2); //True
+console.log("a" == 2); //False
+console.log( 1 <= "2");//True
+console.log("3" >= 2); //True
+console.log("3" > 2);  //True 
+
+//? NULL COMPARISION :
+//* Relational Comparison [null converts to 0] 
+console.log(null > 0);  //False 
+console.log(null < 0);  //False
+console.log(null >= 0); //True
+console.log(null <= 0); //True
+
+//! SPECIAL CASE 
+//* Loose Equality Comparision [null doesnt convert to 0] 
+console.log(null == 0); //False 
+console.log(null != 0); //True 
+
+//? UNDEFINED COMPARISION [doesnt convert to number]
+console.log(undefined == 0); //False
+console.log(undefined <= 0); //False
+console.log(undefined >= 0); //False
+console.log(undefined < 0);  //False
+console.log(undefined > 0); //False 
+
+console.log(null == undefined); //True
+console.log(null > undefined);  //False 
+
+//? Strict Equality Comparision [compares datatype too] :
+console.log(null === undefined); //False [different datatype]
+console.log(2 === 2); //True [same datatype]
+console.log("2" === 2); 
+console.log(null === 0);
+
+
+
+
+
+
+
+
+
