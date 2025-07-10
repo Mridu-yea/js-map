61beb87104439ba50649466f03daa0b0c461d64f
Mridu <kimpeak.2149@gmail.com>
2025-07-10 18:23:21 +0000

    length and indexing done

---
+++ basic/7_string.js
@@ -0,0 +1,23 @@
+name = "mimi"
+points = 102
+console.log(`Points of ${name} are ${points}.`); //* ✔ Correct way to write a string
+
+
+//?---------------------------Length & Indexing---------------------------------------------//
+let line = "I have two 🐈cats"
+
+/* console.log(line.length); //17
+console.log(line.at(-2)); //"t"
+console.log(line.charAt(-2)); //" " => only works on positive index 
+console.log(line.charCodeAt(2)); //104 => Gives unicode | only works on positive index
+console.log(line.codePointAt(12)); */ //56328 => Unicode of symbols , emojis 
+
+//?-------------------------------Searching------------------------------------------------------------//
+let cat = "i love meowfy"
+
+
+
+
+
+
+
