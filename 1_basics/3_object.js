d352cb939d5a1f9ef55de1213576f00396b97d49
Mridu <kimpeak.2149@gmail.com>
2026-05-14 11:08:03 +0000

    objects

---
+++ 1_basics/3_object.js
@@ -0,0 +1,22 @@
+// Objects =>
+const mysym = Symbol("key1")
+    const User = {
+        name : "peak",
+        "email" : "peak@gmail.com",
+        age : 19,
+        [mysym] :"cat",
+        isLoggedin : true
+
+    }
+    User.greet = function(){
+        console.log(`Hello ${this.name}`)
+    }
+    Object.freeze(User)
+    User.age = 20;
+    console.log(User["email"]);
+    console.log(User.isLoggedin);
+    console.table([User]);
+    console.log(User.greet());
+    
+    
+    
