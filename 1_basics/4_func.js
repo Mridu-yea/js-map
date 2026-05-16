47d6e99c3952381088ec50bde83da22862f22af7
Mridu <kimpeak.2149@gmail.com>
2026-05-16 12:02:49 +0000

    scope

--- 1_basics/4_func.js
+++ 1_basics/4_func.js
@@ -1,15 +1,56 @@
 // cart functions =>
- const cart = ["apple" , "banana"]
+    const user = {
+        name : "peak",
+    }
 
+    const item1 = {
+        name : "cupcake",
+        id : 1
+    }
+    const item2 = {
+        name : "cake",
+        id : 2
+    }
+    const item3 = {
+        name : "loaf",
+        id : 3
+    }
+    const item4 = {
+        name : "bread",
+        id : 4
+    }
+    
+    const cart = [item1,item2,item3]
+    console.log(item1);
+    
     function removeItem(){
-        cart.pop()
-        return(cart);
+        const removedItem = cart.pop()
+        return {
+            message : `${removedItem.name} has been removed`,
+            cart : cart 
+        };
         
     }
-    function addItem(item){        
-        cart.push(item);
-        return(  cart);
+    function addItem(product){        
+        cart.push(product);
+        return {
+            message : `${product.name} has been added `,
+            cart : cart
+        };
+        
+    } 
+    console.log( removeItem());
+    console.log(addItem(item4));
+
+    function userinfo (anyobject , arr){
+        console.log( `Total cart items of ${anyobject.name} : `);
+    }
+    
+    function arr(getArray){
+        console.log(getArray);
         
     }
-    console.log(" Item removed : ", removeItem());
-    console.log("Item added : " ,addItem("watermelon"));
+    userinfo(user);
+    arr(cart);
+    
+
