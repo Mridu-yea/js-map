be4b708dc763199c3fb616e17523586a429426d4
Mridu <kimpeak.2149@gmail.com>
2026-05-20 11:26:38 +0000

    moved files

--- 1_basics/5_scope.js
+++
@@ -1,9 +0,0 @@
-// global scope 
-let a = 5 // global 
-function addone(num){
-    let b = 4 //local 
-    return a+b;
-}
-console.log(addone(5));
-//console.log(a+b) => b is not defined 
-// same in functions , if else , objects
