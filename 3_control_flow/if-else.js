388d982f7860b11868d92134a70956c71322fd84
Mridu <kimpeak.2149@gmail.com>
2026-05-20 12:01:01 +0000

    terniary

--- 3_control_flow/if-else.js
+++ 3_control_flow/if-else.js
@@ -22,3 +22,16 @@
         console.log("logged in as guest");
         
     }
+
+// Nullish coalescing operation : always takes case other than null or undef
+
+let val;
+val = 5 ?? 10;        //10
+val = null ?? 5;      //5
+val = undefined ?? 5; //5
+
+//Terniary operator : condition ? true : false 
+
+const points = 100;
+points >= 100 ? console.log("You can buy items now") : console.log("Not enough points");
+
